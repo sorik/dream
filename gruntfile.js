8ed89e52d944eb9843a2301cb7a8a191b034f868
Sori Kang <sorikuya@gmail.com>
2015-04-14 09:40:21 +1000

    add grunt run for running app

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig ({
-    });
+    run: {
+      app: {
+        args: [
+          'src/app.js'
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-run');
+
+  grunt.registerTask('serve', ['run:app']);
 };

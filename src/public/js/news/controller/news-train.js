e0eb132165cbf7d7a0ec48d2cd6aa572e8f6b5d8
Sori Kang <sorikuya@gmail.com>
2015-04-24 11:43:58 +1000

    make training works. use ng-switch to toggle the training view

--- src/public/js/news/controller/news-train.js
+++ src/public/js/news/controller/news-train.js
@@ -2,22 +2,25 @@ angular.module('myNews')
   .controller('newsTrainCtrl', ['$scope', '$stateParams', 'NewsService', function($scope, $stateParams, NewsService) {
     'use strict';
 
+    $scope.startTraining = function () {
+      $scope.userAction = "training";
+    };
+
     NewsService.getById($stateParams.id)
     .then(function(data) {
         $scope.news = data;
-        $scope.todayWords = $scope.news.content;
+        $scope.todayWords = $scope.news.content.split(/\s+/);
+        $scope.todayTraining = $scope.news.content.split(/\s+/);
     });
 
-    $scope.showTrain = function() {
-      $scope.hideTrain = false;
-    };
-
-    $scope.hideTrainWord = function(index) {
+    $scope.selectWord = function(index) {
       $scope.todayTraining[index] = '____';
+      $scope.selected = true;
     };
 
     $scope.showTrainWord = function(index) {
       $scope.todayTraining[index] = $scope.todayWords[index];
+      $scope.answered = true;
     };
   }])
   .directive('newsWord', function(){
@@ -27,17 +30,17 @@ angular.module('myNews')
       transclude: true,
       scope: {
         word: '=',
-        clicked: '&hideWord'
+        clicked: '&selectWord'
 
       },
-      template: '<a ng-click="clicked()">{{word}} </a>'
+      template: '<a ng-click="clicked()" ng-class="{selected: selected}"">{{word}} </a>'
     };
   })
   .directive('newsOriginal', function() {
     'use strict';
     return {
       restrict: 'E',
-      template: '<news-word hide-word="hideTrainWord($index)" ng-repeat="word in todayWords track by $index" word="word"></news-word>'
+      template: '<news-word select-word="selectWord($index)" ng-repeat="word in todayWords track by $index" word="word"></news-word>'
     };
   })
   .directive('newsTrainingWord', function(){
@@ -49,21 +52,14 @@ angular.module('myNews')
         word: '=',
         clicked: '&showWord'
       },
-      template: '<a ng-click="clicked()">{{word}} </a>'
-    };
-  })
-  .directive('newsTrainingStart', function(){
-    'use strict';
-    return {
-      restrict: 'E',
-      template: '<a href ng-click="showTrain()">Start Train</a>'
+      template: '<a ng-click="clicked()" ng-class="{answered: answered}">{{word}} </a>'
     };
   })
   .directive('newsTraining', function(){
     'use strict';
     return {
       restrict: 'E',
-      template: '<news-training-word show-word="showTrainWord($index)"ng-hide="hideTrain" ng-repeat="word in todayTraining  track by $index" word="word"></news-training-word>'
+      template: '<news-training-word show-word="showTrainWord($index)" ng-hide="hideTrain" ng-repeat="word in todayTraining track by $index" word="word"></news-training-word>'
     };
   });
 

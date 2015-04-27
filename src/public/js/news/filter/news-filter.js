ae32e06b642dd941791b091ae3aa3a99671de763
Sori Kang <sorikuya@gmail.com>
2015-04-27 16:19:06 +1000

    remove pagination filter because not using anylonger

--- src/public/js/news/filter/news-filter.js
+++
@@ -1,7 +0,0 @@
-angular.module('myNews')
-    .filter('pagination', function() {
-        return function(inputArray, page, pageSize) {
-            var start = page * pageSize;
-            return inputArray.slice(start, start + pageSize);
-        };
-    });
